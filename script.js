f470f570e2dba7e799ed9fb775666e74f5996c2b
Haytham Khalil <haytham.khalil.eng@gmail.com>
2026-03-13 22:24:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", () => {
+  const msg = document.getElementById("msg");
+  msg.innerText = "✅ JavaScript is working! CI/CD pipeline deployed this successfully.";
+});
